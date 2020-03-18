dfa5dff23031d5efbfe364b7055de84f0021dc29
siyuannnnn-hsy <1587935941@qq.com>
2020-03-18 10:12:46 +0800

    spa

---
+++ 31-echart/rectangle.js
@@ -0,0 +1,40 @@
+/* global $: true */
+$(function () {
+    var myChart = echarts.init(document.getElementById('main'));
+
+    var xData = [],
+        yData = [];
+
+    for(var p = 0;p<=1;p+=0.1){
+        xData.push(p);
+        if( p===0 || p===1 ){
+            yData.push(0)
+        }else{
+            yData.push(-1*p*Math.log2(p)-(1-p)*Math.log2(1-p))
+        }
+        
+    }
+    
+    var option = {
+        title: {
+            text: '二进熵函数'
+        },
+        tooltip: {},
+        legend: {
+            data:['信息量']
+        },
+        xAxis: {
+            data: xData
+        },
+        yAxis: {},
+        series: [{
+            name: '信息量',
+            type: 'line',
+            smooth: true,
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    myChart.setOption(option);
+        
+});
---
+++ 33-hightlight/rectangle.js
@@ -0,0 +1,14 @@
+/* global $: true */
+$(function () {
+    $('input').click(function () { 
+        var $code = $('<div><pre class="javascript"></pre></div>')
+        var $txt = $('textarea');
+
+        if($txt.val() !== '') {
+            $code.find('pre').html($txt.val())
+            hljs.highlightBlock($code.find('pre')[0]);
+            $('body').append($code);
+        }
+    })
+        
+});
